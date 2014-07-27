8e02c84e6451cfa18e576488770e04444562b19f
David Rojas <drojascamaggi@gmail.com>
2014-07-27 19:48:22 -0400

    change build target dir name to "build"

--- gulpfile.js
+++ gulpfile.js
@@ -9,37 +9,48 @@ var webpack       = require('gulp-webpack');
 var ext_replace   = require('gulp-ext-replace');
 var clientConfig  = require('./webpack.client-config.js');
 
+// common paths
 var paths = {
   client: {
-    dir: 'src/js/client',
-    src: 'src/js/client/index.js',
-    jsx: 'src/js/client/**/*.jsx',
-    css: 'src/js/client/**/*.css',
-    dest: 'assets/'
+    directoryName:       'src/js/client',
+    entryPoint:          'src/js/client/index.js',
+    jsxFiles:            'src/js/client/**/*.jsx',
+    cssFiles:            'src/js/client/**/*.css',
+    targetDirectoryName: 'build/assets',
+    styleAssetName:      'style.css'
   }
 };
 
-gulp.task('clean', function(cb) {
-  del(['assets'], cb);
+// clean client build output
+gulp.task('clean-client', function(cb) {
+  del([path.join(paths.client.targetDirectoryName, clientConfig.output.filename)], cb);
 });
 
-gulp.task('client', ['css', 'jsx'], function() {
-  return gulp.src(paths.client.src)
+// build the app after taking care of css and jsx
+gulp.task('build-client', ['clean-client', 'build-css', 'compile-jsx'], function() {
+  return gulp.src(paths.client.entryPoint)
     .pipe(webpack(clientConfig))
-    .pipe(gulp.dest(paths.client.dest));
+    .pipe(gulp.dest(paths.client.targetDirectoryName));
 });
 
-gulp.task('jsx', function() {
-  return gulp.src(paths.client.jsx)
+// compile jsx files in-place
+gulp.task('compile-jsx', function() {
+  return gulp.src(paths.client.jsxFiles)
     .pipe(react())
     .pipe(ext_replace('.js'))
-    .pipe(gulp.dest(paths.client.dir))
+    .pipe(gulp.dest(paths.client.directoryName))
 });
 
-gulp.task('css', function() {
-  return gulp.src(paths.client.css)
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest(paths.client.dest))
+// clean css build output
+gulp.task('clean-css', function(cb) {
+  del([path.join(paths.client.targetDirectoryName, paths.client.styleAssetName)], cb);
 });
 
-gulp.task('default', ['clean', 'client']);
+// concat all css files from app into a main style
+gulp.task('build-css', ['clean-css'], function() {
+  return gulp.src(paths.client.cssFiles)
+    .pipe(concat(paths.client.styleAssetName))
+    .pipe(gulp.dest(paths.client.targetDirectoryName))
+});
+
+gulp.task('default', ['build-client']);
